80d1165c1f5f868c50b7a9107f04b0975b2be93d
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-10 07:17:13 +0100

    Use the ppx for applicative.

--- examples/basics.ml
+++ examples/basics.ml
@@ -1,10 +1,17 @@
 open! Base
 open! Dataframe
+open Df.Applicative.Let_syntax
 
 let () =
   let pi = Column.of_array Native_array.int [| 3; 1; 4; 1; 5; 9; 2; 6; 5 |] in
   let e = Column.of_array Native_array.float [| 2.; 7.; 1.; 8.; 2.; 8.; 1.; 8.; 2. |] in
   let df = Df.create_exn [ "pi", P pi; "e", P e ] in
   List.iter (Df.to_aligned_rows df) ~f:Stdio.print_endline;
-  let df = Df.filter df Df.Applicative.(apply (return (fun pi -> pi = 1)) (int "pi")) in
+  let df =
+    Df.filter
+      df
+      [%map_open
+        let pi = Df.Applicative.int "pi" in
+        pi = 1]
+  in
   List.iter (Df.to_aligned_rows df) ~f:Stdio.print_endline
