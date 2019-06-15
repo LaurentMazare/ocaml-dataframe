dfbcc7eb8702349bc0c2242b4d601f2bd7aef74c
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-15 16:40:11 +0100

    Optional column specs when reading a csv file.

--- src/dataframe/df_csv.ml
+++ src/dataframe/df_csv.ml
@@ -1,21 +1,73 @@
 open Base
 
-let read filename =
-  let csv = Csv.load filename in
-  if Csv.columns csv = 0
-  then Or_error.errorf "no columns in csv file %s" filename
-  else (
-    let lines = Csv.lines csv in
-    if lines = 0
-    then Or_error.errorf "no rows in csv file %s" filename
-    else (
-      let csv = Csv.transpose csv in
-      List.map csv ~f:(function
-          | [] -> assert false (* should be caught by earlier checks *)
-          | header :: data ->
-            let column = Column.of_array Native_array.string (List.to_array data) in
-            header, Column.P column)
-      |> Df.create))
+let read ?columns filename =
+  let open Or_error.Let_syntax in
+  let%bind csv = Or_error.try_with (fun () -> Csv.load filename) in
+  let%bind () =
+    if Csv.columns csv <> 0
+    then Ok ()
+    else Or_error.errorf "no columns in csv file %s" filename
+  in
+  let lines = Csv.lines csv in
+  let%bind () =
+    if lines > 0 then Ok () else Or_error.errorf "no rows in csv file %s" filename
+  in
+  let csv = Csv.transpose csv in
+  let%bind columns =
+    match columns with
+    | None ->
+      let columns =
+        List.map csv ~f:(function
+            | [] -> Or_error.errorf "no data found in %s" filename
+            | header :: data ->
+              let column = Column.of_array Native_array.string (List.to_array data) in
+              Ok (header, Column.P column))
+      in
+      Ok columns
+    | Some columns ->
+      let%bind () =
+        let column_names = List.map columns ~f:fst in
+        match List.find_all_dups column_names ~compare:String.compare with
+        | [] -> Ok ()
+        | dups -> Or_error.errorf "duplicate columns %s" (String.concat dups ~sep:",")
+      in
+      let csv_columns =
+        List.map csv ~f:(function
+            | [] -> Or_error.errorf "no data found in %s" filename
+            | header :: data -> Ok (header, data))
+      in
+      let%bind csv_columns = Or_error.all csv_columns in
+      let%bind csv_columns =
+        match Map.of_alist (module String) csv_columns with
+        | `Ok map -> Ok map
+        | `Duplicate_key key -> Or_error.errorf "duplicate header %s in %s" key filename
+      in
+      let columns =
+        List.map columns ~f:(fun (name, Array_intf.P a) ->
+            match Map.find csv_columns name with
+            | None -> Or_error.errorf "cannot find column %s in %s" name filename
+            | Some data ->
+              Or_error.try_with (fun () ->
+                  let (module M) = a in
+                  let data =
+                    List.to_array data
+                    |> Array.map ~f:(fun elt ->
+                           match M.Elt.of_string elt with
+                           | None ->
+                             Printf.failwithf
+                               "cannot parse %s in column %s when reading %s"
+                               elt
+                               name
+                               filename
+                               ()
+                           | Some v -> v)
+                  in
+                  name, Column.P (Column.of_array a data)))
+      in
+      Ok columns
+  in
+  let%bind columns = Or_error.all columns in
+  Df.create columns
 
 let read_exn filename = Or_error.ok_exn (read filename)
 
--- src/dataframe/df_csv.mli
+++ src/dataframe/df_csv.mli
@@ -1,11 +1,24 @@
-(* TODO: handle explicit headers in [Csv.read] ?
-   TODO: automatic cast for [Csv.read].
+(* TODO: automatic cast for [Csv.read].
 *)
 open Base
 
 (** {3 CSV Reading and Writing } *)
 
-val read : string -> [ `unfiltered ] Df.t Or_error.t
+(** [read ?columns filename] loads a dataframe from a CSV file.
+    If [columns] is specified, only the named columns are used in the
+    returned dataframe. If some columns are missing from the CSV file
+    an error is returned.
+*)
+val read
+  :  ?columns:(string * Array_intf.packed) list
+  -> string
+  -> [ `unfiltered ] Df.t Or_error.t
+
+(** Similar to [read_exn] but raises exception on errors. *)
 val read_exn : string -> [ `unfiltered ] Df.t
+
+(** [write df filename] writes dataframe [df] in CSV format. *)
 val write : _ Df.t -> string -> unit Or_error.t
+
+(** Similar to [write_exn] but raises exception on errors. *)
 val write_exn : _ Df.t -> string -> unit
