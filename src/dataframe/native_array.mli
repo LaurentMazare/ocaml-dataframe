61a2dcf2c7b26e80c6636bc223460230cd8982bc
Laurent Mazare <laurent.mazare@gmail.com>
2019-06-27 12:57:21 +0100

    Update native_array.mli

--- src/dataframe/native_array.mli
+++ src/dataframe/native_array.mli
@@ -1,5 +1,5 @@
-module Make(E: Array_intf.Elt) : Array_intf.S with type Elt.t = E.t
-module MakeOption(E: Array_intf.Elt) : Array_intf.S with type Elt.t = E.t option
+module Make(E: Array_intf.Elt) : Array_intf.S with type Elt.t = E.t and type t = E.t array
+module MakeOption(E: Array_intf.Elt) : Array_intf.S with type Elt.t = E.t option and type t = E.t Base.Option_array.t
 
 val int : (int, int array) Array_intf.t
 val float : (float, float array) Array_intf.t
